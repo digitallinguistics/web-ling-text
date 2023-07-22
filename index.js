30cc889eb27fe045897dd770e5dc622eb1f26247
Daniel W. Hieber <dwhieb@gmail.com>
2023-07-21 23:29:55 -0500

    NEW: Querystrings

--- index.js
+++ index.js
@@ -1,33 +1,81 @@
 const { clipboard } = navigator
 const output        = document.getElementById(`output`)
 
-function convertText(ev) {
+const markdown = window.markdownit({
+  quotes     : '“”‘’',
+  typographer: true,
+})
 
-  const text     = ev.clipboardData.getData(`text/plain`)
-  const html     = ev.clipboardData.getData(`text/html`)
+function convertText(text) {
+
   const template = document.createElement(`template`)
 
-  template.innerHTML = html || text
+  template.innerHTML = text
 
   const italicNodes = template.content.querySelectorAll(`em`)
 
   for (const node of italicNodes) {
-    node.replaceWith(`“${ node.textContent }”`)
+    node.replaceWith(`“${node.textContent}”`)
   }
 
   const strongNodes = template.content.querySelectorAll(`strong`)
 
   for (const node of strongNodes) {
-    node.replaceWith(`*${ node.textContent }*`)
+    node.replaceWith(`*${node.textContent}*`)
   }
 
-  output.innerHTML   = ``
-  output.appendChild(template.content)
-
-  smartquotes()
-
-  clipboard.writeText(output.innerText)
+  return template
 
 }
 
-addEventListener(`paste`, convertText)
+function displayText(template) {
+  output.innerHTML = ``
+  output.appendChild(template.content)
+  clipboard.writeText(output.innerText)
+}
+
+function handleQuerystring() {
+
+  const { searchParams } = new URL(window.location)
+
+  if (!searchParams.size) return
+
+  const md          = searchParams.get(`text`)
+  const redirectURI = searchParams.get(`redirect_uri`)
+
+  if (md === null) {
+
+    const error = `No text was provided to convert.`
+
+    if (redirectURI) {
+      window.location = `${ redirectURI }?error=${error}`
+    }
+
+    return
+
+  }
+
+  const html     = markdown.render(md)
+  const template = convertText(html)
+
+  displayText(template)
+
+  if (redirectURI) {
+    window.location = `${ redirectURI }?text=${ output.value }`
+  }
+
+}
+
+function handlePaste(ev) {
+
+  const text   = ev.clipboardData.getData(`text/plain`)
+  const html   = ev.clipboardData.getData(`text/html`)
+
+  const template = convertText(html || text)
+
+  displayText(template)
+
+}
+
+addEventListener(`load`, handleQuerystring)
+addEventListener(`paste`, handlePaste)
--- test.md
+++ test.md
@@ -5,3 +5,6 @@
 The best introduction to linguistics is "How languages work".
 
 This is a test.
+
+Link:
+<http://localhost:3000/?text=Here%27s%20a%20_description_%20with%20some%20**content**.&redirect_uri=https://trello.com/c/RRylKtwz>
